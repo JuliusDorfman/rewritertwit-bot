28e3b8a07016b8dfe44325c20bd7726f48312e87
JuliusDorfman <juliusdorfman@gmail.com>
2020-01-21 17:54:34 -0800

    updated readme added whatilearned.md

---
+++ WhatILearned.md
@@ -0,0 +1,13 @@
+# Goal
+
+- To create a top quality gimmick teir TwitterBot that would make unexpectant users laugh with an optional aestheticly paired UI for presenting purposes.
+
+## Most Difficult Hurdles
+
+- Time Limit soft date set for 1 month Jan 1st to Jan 17th.
+
+- Working within technologies and 3rd party limitations such as server request limits is actually harder than the logic and data manipulation at least for smaller projects like this one.
+
+- After having not used HTML and CSS for a year, it was difficult to recall certain widely used techniques for positioning.
+
+- Asyncronous api calls to 3rd party services mixed with functions that move syncronously. E.G. populating a string dynamically where certain words are dependent on a database and algorithm and others are non-changed.
--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div className = "header">
+        <div className="header">
             {/* Logo */}
-            <Link className = "nav-title" to="/">
+            {/* <Link className = "nav-title" to="/">
                 <img className = "nav-logo" src={ "/logo192.png" } alt="React logo" />
-            </Link>
-
+            </Link> */}
+            <h1>Welcome to the Eloquent Twitter Stream</h1>
+            <h2>by ReWriter, Twit</h2>
+            <h4>I rewrite and transform your borderline inarticulate Tweets into something beautiful.</h4>
             {/* Page Links */}
-            <div className = "nav-items">
-                <Link className = "nav-link" to='/Home'>Home</Link>
+            <div className="nav-items">
+                {/* <Link className="nav-link" to='/Home'>Home</Link> */}
                 {/* <Link className = "nav-link" to='/Register'>Extra Page</Link> */}
                 {/* <a className = "nav-link" target='_blank' rel="noopener noreferrer" href="https://reactjs.org/docs/getting-started.html">
                     React Docs
