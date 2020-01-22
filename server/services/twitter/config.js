b77bd1e8ca13a5812216efae31f3a0618e9cc03f
JuliusDorfman <juliusdorfman@gmail.com>
2020-01-21 16:28:21 -0800

    cfg

---
+++ server/services/twitter/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  postingMethod: function() {
+    return postingConfig = {
+      consumer_key: '4WVUzDK96xoX7ya5w0bX7sN4k',
+      consumer_secret: 'nJnqSIckkUf44YRk96FS2GjEggPr2A6jmF79I7OzOIiS5B13ZU',
+      access_token: '1187545095794331648-pykHAMqmpkdqgPY8qcXxoUXSXX969e',
+      access_token_secret: 'p2kpkTCnhYKGfcpu74jxLp12k3cMltDT9qf2HtKSzFs9M'
+    }
+  },
+  streamingMethod: function() {
+    return streamingConfig = {
+      consumer_key: '4WVUzDK96xoX7ya5w0bX7sN4k',
+      consumer_secret: 'nJnqSIckkUf44YRk96FS2GjEggPr2A6jmF79I7OzOIiS5B13ZU',
+      access_token: '1187545095794331648-pykHAMqmpkdqgPY8qcXxoUXSXX969e',
+      access_token_secret: 'p2kpkTCnhYKGfcpu74jxLp12k3cMltDT9qf2HtKSzFs9M',
+      timeout_ms: 50000  //should limit requests by 5 seconds
+      // strictSSL: true     // optional - requires SSL certificates to be valid.
+    }
+  },
+  dictionaryMethod: function() {
+    return dictionaryConfig = {
+    dictionary_key: "825c7e69-3ed4-4be0-8e41-2fa0ecd10e82",
+    thesaurus_key: "b13fb57b-7288-48ef-92aa-341f2b29d386"
+    }
+  }
+}
+
+// dictionary
+// 825c7e69-3ed4-4be0-8e41-2fa0ecd10e82
+
+// thesaurus
+// b13fb57b-7288-48ef-92aa-341f2b29d386
